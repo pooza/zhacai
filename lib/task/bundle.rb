f1c15987531326090e532116d01575339e9fa9ef
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-30 10:42:28 +0900

    #1

---
+++ lib/task/bundle.rb
@@ -0,0 +1,14 @@
+namespace :bundle do
+  desc 'update gems'
+  task :update do
+    sh 'bundle update'
+  end
+
+  desc 'check gems'
+  task :check do
+    unless Zhacai::Environment.gem_fresh?
+      warn 'gems is not fresh.'
+      exit 1
+    end
+  end
+end
