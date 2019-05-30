df94df48a9121f0857f6bb2d6fcec262d470bcc4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-05-30 16:09:00 +0900

    #1

--- lib/zhacai/crawler.rb
+++ lib/zhacai/crawler.rb
@@ -1,12 +1,16 @@
 require 'addressable/uri'
+require 'time'
+require 'uri'
 
 module Zhacai
   class Crawler
     attr_reader :params
 
     def initialize(params)
+      @config = Config.instance
       @params = Config.flatten('', params)
       @logger = Logger.new
+      @http = HTTP.new
     end
 
     def crawl
@@ -25,10 +29,34 @@ module Zhacai
       return template.to_s
     end
 
+    def entries
+      entries = {}
+      @http.get(article_list_uri).parsed_response['pages'].each do |entry|
+        time = Time.parse(entry['updatedAt']).getlocal(tz)
+        uri = create_entry_uri(entry['path'])
+        entries[time.to_s] = {
+          date: time,
+          title: URI.decode_www_form_component(uri.path.split('/').last),
+          uri: uri,
+        }
+      end
+      return entries.sort.reverse
+    rescue => e
+      @logger.error(e)
+    end
+
+    def article_list_uri
+      uri = Addressable::URI.parse(@config['/growi/url'])
+      uri.path = '/_api/pages.list'
+      uri.query_values = {
+        'access_token' => @config['/growi/token'],
+        'path' => @params['/path'],
+      }
+      return uri
+    end
+
     def hook_uri
-      @hook_uri ||= Addressable::URI.parse(@params['/hook'])
-      return nil unless @hook_uri.absolute?
-      return @hook_uri
+      return Addressable::URI.parse(@params['/hook'])
     end
 
     def tags
@@ -44,6 +72,18 @@ module Zhacai
       Config.instance['/growi/entries'].each do |entry|
         yield Crawler.new(entry)
       end
+    end
+
+    private
+
+    def create_entry_uri(path)
+      uri = Addressable::URI.parse(@config['/growi/url'])
+      uri.path = path
+      return uri
+    end
+
+    def tz
+      return Time.now.strftime('%:z')
     end
   end
 end
