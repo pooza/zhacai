3c38cd28ee4faae9cddc5acc6ec4300bf90c9936
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-07 16:17:05 +0900

    ginseng-core 1.4.3

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -1,9 +1,8 @@
-namespace :zhacai do
-  task :test do
-    ENV['TEST'] = Zhacai::Package.name
-    require 'test/unit'
-    Dir.glob(File.join(Zhacai::Environment.dir, 'test/*')).each do |t|
-      require t
-    end
+desc 'test all'
+task :test do
+  ENV['TEST'] = Zhacai::Package.name
+  require 'test/unit'
+  Dir.glob(File.join(Zhacai::Environment.dir, 'test/*')).each do |t|
+    require t
   end
 end
