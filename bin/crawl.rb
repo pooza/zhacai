49dba54bfffa0d794ba0681f0653457c8558ee6e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-17 15:46:54 +0900

    #37

---
+++ bin/crawl.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+path = File.expand_path(__FILE__)
+path = File.expand_path(File.readlink(path)) while File.symlink?(path)
+dir = File.expand_path('../..', path)
+$LOAD_PATH.unshift(File.join(dir, 'app/lib'))
+ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
+
+require 'bundler/setup'
+require 'zhacai'
+require 'optparse'
+
+begin
+  options = ARGV.getopts('', 'entry:')
+  raise 'crawler entry undefined.' unless name = options['entry']
+  unless crawler = Zhacai::Crawler.create(name)
+    raise "crawler '#{name}' undefined."
+  end
+  crawler.exec
+rescue => e
+  warn e.message
+  exit 1
+end
